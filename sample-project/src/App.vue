a194a9402ad7be0573681bc5f45d3dcc5e52f906
iki <iki.jito@gmail.com>
2022-10-24 21:32:46 +0800

    basic set up, rendering from a list using v-for

---
+++ sample-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <h2>{{ title }}</h2>
+    <ul>
+        <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: "Users",
+            users: [
+                {
+                    id: 1231,
+                    name: 'user1',
+                    email: 'user1@example.com'
+                },
+                {
+                    id: 1232,
+                    name: 'user2',
+                    email: 'user2@example.com'
+                }
+            ]
+        }
+    }
+}
+</script>
