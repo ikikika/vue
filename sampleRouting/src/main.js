7e77db914a9446f820b2bc8014f48db4afd2a34f
iki <iki.jito@gmail.com>
2022-10-30 17:19:26 +0800

    organising route files

--- sampleRouting/src/main.js
+++ sampleRouting/src/main.js
@@ -1,72 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList';
-import UsersList from './components/users/UsersList';
-import TeamMembers from './components/teams/TeamMembers';
-import TeamsFooter from './components/teams/TeamsFooter';
-import UsersFooter from './components/users/UsersFooter';
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      redirect: '/teams',
-    },
-    {
-      name: 'teams',
-      path: '/teams',
-      meta: { needsAuth: true },
-      components: { default: TeamsList, footer: TeamsFooter },
-      //   alias: '/',
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true,
-        },
-      ],
-    },
-    {
-      path: '/users',
-      components: { default: UsersList, footer: UsersFooter },
-      //   beforeEnter(_to, _from, next) {
-      // guard can be defined here
-      //   },
-    },
-    // {
-    //   path: '/teams/:teamId',
-    //   component: TeamMembers,
-    //   props: true,
-    // },
-    {
-      path: '/:notFound(.*)',
-      redirect: '/',
-    },
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(_to, _from, savedPosition) {
-    // to - going to
-    // from - coming from
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach(function (to, from, next) {
-  console.log(to, from);
-  if (to.meta.needsAuth) {
-    console.log('needs auth');
-    next();
-  } else {
-    next();
-  }
-});
+import router from './router.js';
 
 const app = createApp(App);
 
