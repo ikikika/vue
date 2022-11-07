ced25d37162af171c75af6a3edb2ff1206707d1e
iki <iki.jito@gmail.com>
2022-11-07 22:15:56 +0800

    pull out store into a separate file

---
+++ sampleVuex/src/store.js
@@ -0,0 +1,73 @@
+import { createStore } from 'vuex';
+
+const counterModule = {
+  namespaced: true, // detach this module from the rest of the store.
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter++;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+  },
+  actions: {
+    increment(context) {
+      context.commit('increment');
+    },
+    increase(context, payload) {
+      setTimeout(function () {
+        context.commit('increase', payload);
+      }, 2000);
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter.toFixed(2);
+    },
+    nomalisedCounter(_, getters) {
+      if (getters.finalCounter > 100) {
+        return 100;
+      }
+      return getters.finalCounter;
+    },
+  },
+};
+
+const store = createStore({
+  modules: {
+    numbers: counterModule,
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth;
+    },
+  },
+  actions: {
+    // actions can run async code, mutations cannot
+    // dun update state in actions. update state in mutations
+
+    login(context) {
+      context.commit('setAuth', { isAuth: true });
+    },
+    logout(context) {
+      context.commit('setAuth', { isAuth: false });
+    },
+  },
+  getters: {
+    userIsAuthenticated(state) {
+      return state.isLoggedIn;
+    },
+  },
+});
+
+export default store;
