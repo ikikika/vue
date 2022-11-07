88ad9161b8beb8fa4a21f2f96d54fa093b50af6b
iki <iki.jito@gmail.com>
2022-11-07 16:55:29 +0800

    using getters in another getter

--- sampleVuex/src/main.js
+++ sampleVuex/src/main.js
@@ -21,6 +21,12 @@ const store = createStore({
     finalCounter(state) {
       return state.counter.toFixed(2);
     },
+    nomalisedCounter(_, getters) {
+      if (getters.finalCounter > 100) {
+        return 100;
+      }
+      return getters.finalCounter;
+    },
   },
 });
 
