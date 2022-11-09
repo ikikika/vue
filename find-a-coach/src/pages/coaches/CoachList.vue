f10872fd98ef61b043a33042eb882772bd8782b7
iki <iki.jito@gmail.com>
2022-11-09 21:31:50 +0800

    wiring up pages

--- find-a-coach/src/pages/coaches/CoachList.vue
+++ find-a-coach/src/pages/coaches/CoachList.vue
@@ -1,7 +1,14 @@
 <template>
     <div>CoachList</div>
     <section>filter</section>
-    <section>list of coaches</section>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Coach</router-link>
+        </div>
+
+        list of coaches
+    </section>
 </template>
 
 <script>
