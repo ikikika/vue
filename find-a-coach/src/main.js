cf8db7dd7264d3ef6a2ef706f4124f5dce8770f8
iki <iki.jito@gmail.com>
2022-11-12 22:02:00 +0800

    loading spinner

--- find-a-coach/src/main.js
+++ find-a-coach/src/main.js
@@ -6,6 +6,7 @@ import store from './store';
 import BaseCard from './components/ui/BaseCard';
 import BaseButton from './components/ui/BaseButton';
 import BaseBadge from './components/ui/BaseBadge';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
 
 const app = createApp(App);
 
@@ -15,5 +16,6 @@ app.use(store);
 app.component('base-card', BaseCard); // available globally to all components
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
 
 app.mount('#app');
