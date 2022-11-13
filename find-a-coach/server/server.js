ffc7497a90dd74ccfcfe2223c8f8b63e7482ef85
iki <iki.jito@gmail.com>
2022-11-14 04:39:19 +0800

    create simple server

---
+++ find-a-coach/server/server.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var fs = require('fs');
+
+// Create a server object
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    var url = req.url;
+
+    if (url === '/coaches') {
+      if (req.method == 'GET') {
+        fs.readFile('./coaches.json', (err, data) => {
+          res.end(JSON.stringify(JSON.parse(data), null, 3));
+        });
+      } else if (req.method == 'POST') {
+        fs.readFile('./coaches.json', (err, data) => {
+          var coachData = JSON.parse(data);
+
+          req.on('data', function (chunk) {
+            var receivedData = JSON.parse(chunk);
+            receivedData.id = Date.now();
+            coachData.push(receivedData);
+
+            fs.writeFile(
+              './coaches.json',
+              JSON.stringify(coachData),
+              function () {
+                res.end('ok');
+              }
+            );
+          });
+        });
+      }
+    } else if (url === '/contact') {
+      res.write(' Welcome to contact us page');
+      res.end();
+    } else {
+      res.write('Hello World!');
+      res.end();
+    }
+  })
+  .listen(8000, function () {
+    // The server object listens on port 3000
+    console.log('server start at port 8000');
+  });
