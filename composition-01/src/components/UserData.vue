0894cec608a16200809390934380775708f083cb
iki <iki.jito@gmail.com>
2022-11-19 18:30:38 +0800

    emit custom events

--- composition-01/src/components/UserData.vue
+++ composition-01/src/components/UserData.vue
@@ -8,10 +8,16 @@ import { computed } from 'vue';
 
 export default {
     props: ['firstName', 'lastName', 'age'],
-    setup(props) {
+    setup(
+        props,
+        // context
+    ) {
         const uName = computed(function () {
             return props.firstName + ' ' + props.lastName;
         });
+
+        // context.emit('save-data', 1); // this.emit('save-data', 1)
+
         return { userName: uName }
     }
 }
